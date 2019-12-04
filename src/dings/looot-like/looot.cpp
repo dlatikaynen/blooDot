4b55b946c274ef812e785d3a00bc47e3445ce1b4
dlatikaynen <dlatikay@outlook.com>
2019-12-04 22:28:43 +0100

    [dlatikay 20191204] chest (no, the other kind)

--- src/dings/looot-like/looot.cpp
+++ src/dings/looot-like/looot.cpp
@@ -41,16 +41,18 @@ Chest::Chest(std::shared_ptr<DX::DeviceResources> deviceResources, BrushRegistry
 void Chest::DrawInternal(Microsoft::WRL::ComPtr<ID2D1BitmapRenderTarget> drawTo)
 {
 	/* declare */
-	D2D1_RECT_F rect;
-	MFARGB colbase { 0, 102, 153, 255 };
-	MFARGB coldark { 0, 91, 143, 255 };
+	D2D1_RECT_F rect;		
 	/* prepare */
 	PrepareRect(&this->m_lookupShy, rect);
 	auto rendEr = drawTo.Get();
-	Microsoft::WRL::ComPtr<ID2D1Brush> brusherl = m_Brushes->WannaHave(drawTo, colbase);
+	Microsoft::WRL::ComPtr<ID2D1Brush> brusherl = m_Brushes->WannaHave(drawTo, MFARGB{ 0, 102, 153, 255 });
 	auto brushBase = brusherl.Get();
-	Microsoft::WRL::ComPtr<ID2D1Brush> brusherl2 = m_Brushes->WannaHave(drawTo, coldark);
+	Microsoft::WRL::ComPtr<ID2D1Brush> brusherl2 = m_Brushes->WannaHave(drawTo, MFARGB{ 0, 91, 143, 255 });
 	auto brushDark = brusherl2.Get();
+	Microsoft::WRL::ComPtr<ID2D1Brush> brusherl3 = m_Brushes->WannaHave(drawTo, MFARGB{ 51, 102, 102, 255 });
+	auto brushBark = brusherl3.Get();
+	Microsoft::WRL::ComPtr<ID2D1Brush> brusherl4 = m_Brushes->WannaHave(drawTo, MFARGB{ 121, 132, 132, 255 });
+	auto brushNail = brusherl4.Get();
 	/* draw away */
 	for (int facing = OrientabilityIndexQuadruplet::Uppy; facing >= OrientabilityIndexQuadruplet::Lefty; --facing)
 	{
@@ -58,10 +60,26 @@ void Chest::DrawInternal(Microsoft::WRL::ComPtr<ID2D1BitmapRenderTarget> drawTo)
 		Rotate(rendEr, rect, facing);
 		auto left = rect.left + 1.0f;
 		auto rite = rect.left + ((rect.right - rect.left) - 4.0f) * blooDot::Consts::GOLDEN_RATIO;
+		auto yctr = rect.top + (rect.bottom - rect.top) / 2.0f;
 		auto step = (rite - left) / 4.0f;
 		rendEr->FillRectangle(D2D1::RectF(left, rect.top + 2.0f, rite, rect.bottom - 2.0f), brushBase);
 		rendEr->DrawLine(D2D1::Point2F(rect.left + step, rect.top + 2.0f), D2D1::Point2F(rect.left + step, rect.bottom - 2.0f), brushDark);
 		rendEr->DrawLine(D2D1::Point2F(rect.left + step * 2.0f, rect.top + 2.0f), D2D1::Point2F(rect.left + step * 2.0f, rect.bottom - 2.0f), brushDark);
 		rendEr->DrawLine(D2D1::Point2F(rect.left + step * 3.0f, rect.top + 2.0f), D2D1::Point2F(rect.left + step * 3.0f, rect.bottom - 2.0f), brushDark);
+		rendEr->FillRectangle(D2D1::RectF(left, rect.top + 3.0f, rite, rect.top + 10.0f), brushBark);
+		rendEr->FillRectangle(D2D1::RectF(left, rect.bottom - 10.0f, rite, rect.bottom - 3.0f), brushBark);
+		auto elli = D2D1::Ellipse(D2D1::Point2F(rect.left + step, rect.top + 6.5f), 1.3f, 1.3f);
+		rendEr->FillEllipse(elli, brushNail);
+		elli = D2D1::Ellipse(D2D1::Point2F(rect.left + step * 2.0f, rect.top + 6.5f), 1.3f, 1.3f);
+		rendEr->FillEllipse(elli, brushNail);
+		elli = D2D1::Ellipse(D2D1::Point2F(rect.left + step * 3.0f, rect.top + 6.5f), 1.3f, 1.3f);
+		rendEr->FillEllipse(elli, brushNail);
+		elli = D2D1::Ellipse(D2D1::Point2F(rect.left + step, rect.bottom - 6.5f), 1.3f, 1.3f);
+		rendEr->FillEllipse(elli, brushNail);
+		elli = D2D1::Ellipse(D2D1::Point2F(rect.left + step * 2.0f, rect.bottom - 6.5f), 1.3f, 1.3f);
+		rendEr->FillEllipse(elli, brushNail);
+		elli = D2D1::Ellipse(D2D1::Point2F(rect.left + step * 3.0f, rect.bottom - 6.5f), 1.3f, 1.3f);
+		rendEr->FillEllipse(elli, brushNail);
+		rendEr->FillRectangle(D2D1::RectF(rite, yctr - 1.8f, rite + 1.5f, yctr + 1.8f), brushNail);
 	}
 }
