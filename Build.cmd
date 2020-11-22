68fd2cd70aab2fc328789d0f94ab1d4c75557c00
dlatikay <53712466+dlatikaynen@users.noreply.github.com>
2020-11-22 17:58:15 +0100

    Create Build.cmd

---
+++ Build.cmd
@@ -0,0 +1,8 @@
+@echo off
+pushd "%~dp0"
+if exist Release rd /s /q Release
+if exist x64 rd /s /q x64
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64
+:exit
+popd
+@echo on
