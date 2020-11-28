70b72d5096123f1e5311a3cd252d09c4a4446696
dlatikay <53712466+dlatikaynen@users.noreply.github.com>
2020-11-28 18:57:32 +0100

    Update Build.cmd

--- .github/workflows/Build.cmd
+++ .github/workflows/Build.cmd
@@ -1,7 +1,7 @@
 @echo off
-cd..
-cd..
 pushd "%~dp0"
+cd..
+cd..
 if exist Release rd /s /q Release
 if exist x64 rd /s /q x64
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64
