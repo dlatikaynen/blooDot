bdf9bed9871c2c3907913fd3c5377d0879c0d97f
dlatikay <latikaynen@loidl-consulting.at>
2022-08-11 01:25:02 +0200

    Use cairo to draw 2d primitives

--- splash.cpp
+++ splash.cpp
@@ -2,6 +2,7 @@
 #include "splash.h"
 #include "dexassy.h"
 #include "scripture.h"
+#include <cairo.h>
 
 extern bool mainRunning;
 extern void ReportError(const char*, const char*);
@@ -51,6 +52,31 @@ void LoadSplash(SDL_Renderer* renderer)
 	}
 
 	splashRunning = true;
+}
+
+cairo_t* cb(cairo_t* cr)
+{
+	cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+	cairo_rectangle(cr, 10, 20, 128, 128);
+	cairo_stroke(cr);
+	return cr;
+}
+
+cairo_t* cai(SDL_Renderer* r, cairo_t* (*f)(cairo_t*))
+{
+	int width = 640, height = 640, pitch; void* pixels;
+	
+	const auto t = SDL_CreateTexture(r, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+	SDL_LockTexture(t, NULL, &pixels, &pitch);
+	const auto cs = cairo_image_surface_create_for_data(static_cast<unsigned char*>(pixels), CAIRO_FORMAT_ARGB32, width, height, pitch);
+	const auto s = cairo_create(cs);
+	const auto fr = f(s);
+	
+	SDL_UnlockTexture(t);
+	SDL_RenderCopy(r, t, NULL, NULL);
+	SDL_RenderPresent(r);
+	
+	return fr;
 }
 
 void SplashLoop(SDL_Renderer* renderer)
@@ -132,6 +158,9 @@ void SplashLoop(SDL_Renderer* renderer)
 		};
 		
 		SDL_RenderCopy(renderer, textTexture, NULL, &titleRect);
+
+		cai(renderer, cb);
+
 		SDL_RenderPresent(renderer);
 		SDL_Delay(16);
 	}
