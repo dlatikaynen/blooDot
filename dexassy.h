0463688be9fda1bb4f2761dba4617bde5fa568ea
dlatikay <latikaynen@loidl-consulting.at>
2022-08-14 15:06:10 +0200

    Added some artwork

--- dexassy.h
+++ dexassy.h
@@ -6,6 +6,18 @@ constexpr auto CHUNK_KEY_FONT_TITLE = 1;
 constexpr auto CHUNK_KEY_FONT_DIALOG = 2;
 constexpr auto CHUNK_KEY_FONT_ALIEN = 3;
 constexpr auto CHUNK_KEY_SFX_PROJECTILEDECAY = 4;
+constexpr auto CHUNK_KEY_DINGS_BARREL_INDIGO = 5;
+constexpr auto CHUNK_KEY_DINGS_BARREL_WOODEN = 6;
+constexpr auto CHUNK_KEY_DINGS_CHEL_F = 7;
+constexpr auto CHUNK_KEY_DINGS_FLOORROCKTILE_CRACKED = 8;
+constexpr auto CHUNK_KEY_DINGS_FLOORROCKTILE = 9;
+constexpr auto CHUNK_KEY_DINGS_FLOORSTONETILE_OCHRE = 10;
+constexpr auto CHUNK_KEY_DINGS_FLOORSTONETILE_PURPLE = 11;
+constexpr auto CHUNK_KEY_DINGS_FLOORSTONETILE_SLATE = 12;
+constexpr auto CHUNK_KEY_DINGS_GRASS = 13;
+constexpr auto CHUNK_KEY_DINGS_LETTUCE = 14;
+constexpr auto CHUNK_KEY_DINGS_SCHAED = 15;
+constexpr auto CHUNK_KEY_DINGS_SNOW = 16;
 
 extern void ReportError(const char*, const char*);
 
--- res/xassy.cmd
+++ res/xassy.cmd
@@ -6,5 +6,17 @@ copy ^
 + /b FreckleFace-Regular.ttf ^
 + /b esl_gothic_shavian.ttf ^
 + /b projectile-decay.wav ^
++ /b barrel-indigo.png ^
++ /b barrel-wooden.png ^
++ /b chel-f.png ^
++ /b floorrocktile-cracked.png ^
++ /b floorrocktile.png ^
++ /b floorstonetile-ochre.png ^
++ /b floorstonetile-purple.png ^
++ /b floorstonetile-slate.png ^
++ /b grass.png ^
++ /b lettuce.png ^
++ /b schaedel.png ^
++ /b snowblock.png ^
   /V /Y ^
 xassy.cooked1.ngld
