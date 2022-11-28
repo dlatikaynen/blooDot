a7702ae6d5268096c437a162d9017baddd304d33
dlatikay <latikaynen@loidl-consulting.at>
2022-11-28 22:00:12 +0100

    Petal logic more intricate than we thought

--- playerstate.cpp
+++ playerstate.cpp
@@ -1,8 +1,6 @@
 #include "pch.h"
 #include "playerstate.h"
 
-#define MAX_VITAL_VALUE ((char)0xf)
-
 namespace blooDot::Player
 {
 	int NumPlayers = 0;
@@ -17,14 +15,38 @@ namespace blooDot::Player
 	void VitalIncrease(MobState* mobState, VitalSlotDedication dedication)
 	{
 		Uint16 ffp2 = 0b0000000000001111 << (4 * dedication);
+		Uint16 capaMask = 0b0000000000000111 << (3 * dedication);
+		Uint16 dediMask = 0b0000000000000011 << (2 * dedication);
+		auto capacity = (mobState->Capacity & capaMask) >> (3 * dedication);
 		auto currentValue = static_cast<char>((mobState->Vitals & ffp2) >> (4 * dedication));
-		if (currentValue < MAX_VITAL_VALUE)
+		if (currentValue < capacity)
 		{
 			++currentValue;
 			mobState->Vitals = (mobState->Vitals & ~ffp2) + (currentValue << (4 * dedication));
+			if (currentValue == 1)
+			{
+				/*
+				* do we refill a previously repurposed or empty
+				* petal? then we will simply reinizialize the petal
+				* --> assign to it its native dedication
+				*/
+				mobState->SlotDedication = static_cast<Uint8>((mobState->SlotDedication & ~dediMask) + (dedication << (2 * dedication)));
+			}
+			else
+			{
+
+				/* do we spill over?
+				 * (in which case we kill the spilled-into petal
+				 * unless we're already back around */
+			}
 		}
 	}
 
+	/// <summary>
+	/// Decrease cannot alter slot dedication.
+	/// An empty slot is only ever refilled if the mob
+	/// picks up one of its original dedication
+	/// </summary>
 	void VitalDecrease(MobState* mobState, VitalSlotDedication dedication)
 	{
 		Uint16 ffp2 = 0b0000000000001111 << (4 * dedication);
--- playerstate.h
+++ playerstate.h
@@ -28,9 +28,15 @@ typedef struct MobStateStruct
 	// bits 0..1 --> NE petal, 2..3 --> NW petal, 4..5 --> SW petal, 6..7 --> SE petal
 	Uint8 SlotDedication; 
 
+	// bits 0..2 --> phys, 3..5 --> heat, 6..8 --> cold, 9..11 --> chem
+	Uint16 Capacity;
+
 	// bits 0..3 --> phys, 4..7 --> heat, 8..11 --> cold, 12..15 --> chem
-	Uint16 Vitals;
+	Uint16 Vitals; // conveniency property for gameplay. value is redundant via petals x dedication
 	
+	// bits 0..2 --> NE petal, 3..5 --> NW petal, 6..8 --> SW petal, 9..11 --> SE petal
+	Uint16 Petals;
+
 	PointInWorld Position;
 	SDL_Point Offset;
 	int Orientation;
