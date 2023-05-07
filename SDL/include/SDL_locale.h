cd7bedf96d4b28cc8930c73838331df9a359139c
dlatikay <latikaynen@loidl-consulting.at>
2023-05-07 17:20:01 +0200

    # - updated to SDL 2.26.5

--- SDL/include/SDL_locale.h
+++ SDL/include/SDL_locale.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2022 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2023 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -42,7 +42,7 @@ extern "C" {
 
 typedef struct SDL_Locale
 {
-    const char *language;  /**< A language name, like "am" for American. */
+    const char *language;  /**< A language name, like "en" for English. */
     const char *country;  /**< A country, like "US" for America. Can be NULL. */
 } SDL_Locale;
 
